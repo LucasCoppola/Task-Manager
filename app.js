114abeab06ddca2481ea8eb98b72fd2c2c81f306
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-04-04 18:50:54 -0300

    port issue

--- app.js
+++ app.js
@@ -130,7 +130,7 @@ app.use((err, req, res, next) => {
 	}
 })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 10000
 
 app.listen(port, () => {
 	console.log('Listening On Port 5000')
